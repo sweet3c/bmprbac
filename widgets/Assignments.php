af55ce023d35e7eaa9e1307223c7c15e9e3e2dfa
lixupeng <lixupeng@55tuan.com>
2015-09-17 14:53:49 +0800

    修改RBAC代码

--- widgets/Assignments.php
+++ widgets/Assignments.php
@@ -21,7 +21,7 @@ use yii\base\Widget;
  * This widget may be used in user update form and provides ability to assign
  * multiple auth items to the user.
  * 
- * @author Dmitry Erofeev <dmeroff@gmail.com>
+ * @author xupengLi <740942943@qq.com>
  */
 class Assignments extends Widget
 {
